ac18f32eb2b1d3e47ba5a3795ea2d95dd8e4d657
bonjourcyn <cynsipes@gmail.com>
2021-08-25 17:05:45 +0100

    HealthBar

---
+++ Safe Space Jam/Assets/Scripts/Volume.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Volume : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+   
+}
